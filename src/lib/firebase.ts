055d1be977f6b7cd9a3d18a2c40e33095fbf58bf
Larry AI <lg.ailogin@gmail.com>
2025-07-05 11:54:31 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,9 +5,9 @@ import { getFirestore, type Firestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAUFUIhPIVJfBbliM8YiYMX1jbRrq4l3_w",
-  authDomain: "project-594504357737.firebaseapp.com",
-  projectId: "project-594504357737",
-  storageBucket: "project-594504357737.appspot.com",
+  authDomain: "myetician.firebaseapp.com",
+  projectId: "myetician",
+  storageBucket: "myetician.appspot.com",
   messagingSenderId: "594504357737",
   appId: "1:594504357737:web:e98ac2c7957b2ff21ccff8",
 };
